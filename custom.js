b6b28b9e951cf83acd589590512d908c8b4871eb
Mahir <mahir@jackbom.com>
2026-03-13 14:29:01 +0300

    https://jackbomtv20.com/ e geçiş yapıldı.

--- custom.js
+++ custom.js
@@ -248,7 +248,7 @@
                 },
                 {
                     name: langMap[language].liveTV,
-                    url: `https://jackbomtv18.com/`,
+                    url: `https://jackbomtv20.com/`,
                     icon: "fa-solid fa-circle-play",
                 },
                 {
@@ -400,7 +400,7 @@
             });
 
             const headerButtons = `
-      	<a class="header-custom-button custom ${isUserLoggedIn === true ? "logged-in" : ""} d-flex px-3 align-items-center text-white" href="https://jackbomtv18.com/" target="_blank">
+      	<a class="header-custom-button custom ${isUserLoggedIn === true ? "logged-in" : ""} d-flex px-3 align-items-center text-white" href="https://jackbomtv120.com/" target="_blank">
           <i class="fa-solid fa-tv"></i>
           Jack TV
         </a>
